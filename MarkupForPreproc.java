0412174f8c029874a432816a640fd7d5b8bdcaff
Eric Chauvin <chauvineric4@gmail.com>
2020-02-25 11:07:35 -0700

    Update MarkupForPreproc.java

--- MarkupForPreproc.java
+++ MarkupForPreproc.java
@@ -9,100 +9,13 @@
  
 
 
-  public class MarkupForPreproc
+public class MarkupForPreproc
   {
 
   public static String MarkItUp( MainApp mApp,
-                                    String fileName )
+                                    String result )
 
     {
-    String showError = "";
-    mApp.showStatus( "File name is: " + fileName );
-    mApp.showStatus( " " );
-
-    // The first level of lexical analysis and
-    // processing is inside FileUtility.java when
-    // it reads the file to a string.
-    String result = FileUtility.readFileToString(
-                                        mApp,
-                                        fileName,
-                                        false );
-
-    if( result.trim().length() == 0 )
-      {
-      mApp.showStatus( "Nothing in Source File." );
-      return "";
-      }
-
-    // mApp.showStatus( result );
-    // Make sure the last line ends with a newline.
-    result = result + "\n";
-
-    // This adds line number markers and also fixes
-    // line splices.
-    result = RemoveStarComments.removeAllComments(
-                                  mApp, result );
-
-    if( result.contains( Character.toString(
-                      Markers.ErrorPoint )))
-      {
-      mApp.showStatus( " " );
-      showError = "There was an error marker after" +
-                               " RemoveStarComments.";
-
-      mApp.showStatus( showError );
-      return "";
-      }
-
-    if( !TestMarkers.testBeginEnd( mApp, result ))
-      {
-      mApp.showStatus( " " );
-      showError = "TestBeginEnd returned false" +
-                        " after RemoveStarComments.";
-      mApp.showStatus( showError );
-      return "";
-      }
-
-    // There can be double-slash comments in
-    // preprocessor lines.
-    result = removeAllDoubleSlashComments( mApp,
-                                           result );
-
-    if( !TestMarkers.testBeginEnd( mApp, result ))
-      {
-      mApp.showStatus( " " );
-      showError = "TestBeginEnd returned false" +
-               " after removeAllDoubleSlashComments.";
- 
-      mApp.showStatus( showError );
-      return "";
-      }
-
-    result = markPreprocessorLines( mApp, result );
-
-    if( result.contains( Character.toString(
-                      Markers.ErrorPoint )))
-      {
-      mApp.showStatus( " " );
-      showError = "There was an error marker after" +
-                               " markPreprocessorLines.";
-
-      mApp.showStatus( showError );
-      return "";
-      }
-
-    if( !TestMarkers.testBeginEnd( mApp, result ))
-      {
-      mApp.showStatus( " " );
-      showError = "TestBeginEnd returned false" +
-                       " after markPreprocessorLine.";
-
-      mApp.showStatus( showError );
-      return "";
-      }
-
-
-    // mApp.showStatus( result );
 
     result = markStrings( mApp, result );
 
@@ -142,10 +55,10 @@
       }
 
 
+    /////////////
     mApp.showStatus( result );
     mApp.showStatus( " " );
-    mApp.showStatus( "Finished processing." );
-    mApp.showStatus( fileName );
+    mApp.showStatus( "Finished markup." );
     mApp.showStatus( " " );
     mApp.showStatus( " " );
     return result;
@@ -187,6 +100,12 @@
       return null;
       }
 
+
+
+
+// After macros replace identifiers and all that.
+// So this below doesn't get done here.
+// This is just markup.
     Result = TestMarkers.RemoveOutsideWhiteSpace( Result );
 
     if( !TestMarkers.TestBrackets( MForm, Result ))
@@ -240,7 +159,7 @@
 
 
 
-  private static String markPreprocessorLines(
+  public static String markPreprocessorLines(
                                       MainApp mApp,
                                       String in )
     {
@@ -317,135 +236,6 @@
     }
 
 
-
-  private static String removeAllDoubleSlashComments(
-                                       MainApp mApp,
-                                       String in )
-    {
-    StringBuilder sBuilder = new StringBuilder();
-    String[] splitS = in.split( "\n" );
-    int last = splitS.length;
-    for( int count = 0; count < last; count++ )
-      {
-      String line = splitS[count];
-      line = removeDoubleSlashComments( mApp, line );
-
-      String startString =
-              Character.toString( Markers.Begin ) +
-              Character.toString( 
-                             Markers.TypeLineNumber );
-
-      // Don't keep empty lines.
-      String tLine = line.trim();
-      if( !tLine.startsWith( startString ))
-        sBuilder.append( line + "\n" );
-
-      }
-
-    return sBuilder.toString();
-    }
-
-
-
-  private static String removeDoubleSlashComments(
-                                        MainApp mApp,
-                                        String line )
-    {
-    // This line with the two back slashes in the
-    // URL should not be considered to be a comment.
-    //     GetPage( "https://gcc.gnu.org/" );
-
-    StringBuilder sBuilder = new StringBuilder();
-
-    line = line.replace( "\\\\",
-         Character.toString( Markers.EscapedSlash ));
-
-    // Notice the escaped forward slash in front of
-    // the quote character here at the end of the
-    // string:
-    // "c:\\BrowserECFiles\\PageFiles\\";
-
-    line = line.replace( "\\\"",
-                        Character.toString( 
-                        Markers.EscapedDoubleQuote ));
-
-    // This double quote inside single quotes can't
-    // be inside of a normal string literal.
-    String singleQuoteCharStr = "\'\"\'";
-    if( singleQuoteCharStr.length() != 3 )
-      {
-      mApp.showStatus( "SingleQuoteCharStr.Length != 3" );
-      return Character.toString( Markers.ErrorPoint );
-      }
-
-    line = line.replace( singleQuoteCharStr,
-                    Character.toString(
-                    Markers.QuoteAsSingleCharacter ));
-
-    String doubleSlash = "/" + "/";
-    line = line.replace( doubleSlash,
-           Character.toString( Markers.DoubleSlash ));
-
-    int lineLength = line.length();
-    boolean isInside = true;
-    boolean isInsideString = false;
-    for( int count = 0; count < lineLength; count++ )
-      {
-      char testChar = line.charAt( count );
-      if( isInsideString )
-        {
-        if( testChar == '"' )
-          isInsideString = false;
-
-        }
-      else
-        {
-        // It's not inside the string.
-        if( testChar == '"' )
-          isInsideString = true;
-
-        }
-
-      if( !isInsideString )
-        {
-        if( testChar == Markers.DoubleSlash )
-          {
-          // This will stay false until it gets to
-          // the Begin marker for the line number.
-          isInside = false;
-          }
-        }
-
-      // This is for the line number markers.
-      if( testChar == Markers.Begin )
-        isInside = true;
-
-      if( isInside )
-        sBuilder.append( testChar );
-
-      }
-
-    String result = sBuilder.toString();
-
-    result = result.replace( 
-            Character.toString( Markers.DoubleSlash ),
-                                doubleSlash );
-
-    result = result.replace(
-                          Character.toString(
-                     Markers.QuoteAsSingleCharacter ),
-                     singleQuoteCharStr );
-
-    result = result.replace( Character.toString(
-                         Markers.EscapedDoubleQuote ),
-                         "\\\"" );
-
-    result = result.replace( Character.toString(
-                             Markers.EscapedSlash ),
-                             "\\\\" );
-
-    return result;
-    }
 
 
 
