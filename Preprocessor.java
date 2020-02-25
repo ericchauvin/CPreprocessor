86354f6b64e7580296d57487457d0f57736e08f1
Eric Chauvin <chauvineric4@gmail.com>
2020-02-25 11:04:31 -0700

    Create Preprocessor.java

---
+++ Preprocessor.java
@@ -0,0 +1,102 @@
+// Copyright Eric Chauvin 2018 - 2020.
+
+
+
+public class Preprocessor
+  {
+
+  public static String PreprocessFile( MainApp mApp,
+                                     String fileName )
+
+    {
+    String showError = "";
+    mApp.showStatus( "Preprocessing file:\n" + fileName );
+
+    // The first level of lexical analysis and
+    // processing is inside FileUtility.java when
+    // it reads the file to a string.
+    String result = FileUtility.readFileToString(
+                                        mApp,
+                                        fileName,
+                                        false );
+
+    if( result.trim().length() == 0 )
+      {
+      mApp.showStatus( "Nothing in Source File." );
+      return "";
+      }
+
+    // mApp.showStatus( result );
+    // Make sure the last line ends with a newline
+    // so that the very last line is not unusual.
+    // All lines end with newlines.
+    result = result + "\n";
+
+    // This adds line number markers and also fixes
+    // line splices.
+    result = RemoveComments.removeAllComments(
+                                  mApp, result );
+
+    if( result.contains( Character.toString(
+                      Markers.ErrorPoint )))
+      {
+      mApp.showStatus( " " );
+      showError = "There was an error marker after" +
+                               " RemoveAllComments.";
+
+      mApp.showStatus( showError );
+      return "";
+      }
+
+    if( !TestMarkers.testBeginEnd( mApp, result ))
+      {
+      mApp.showStatus( " " );
+      showError = "TestBeginEnd returned false" +
+                        " after RemoveAllComments.";
+      mApp.showStatus( showError );
+      return "";
+      }
+
+    result = MarkupForPreproc.markPreprocessorLines(
+                                        mApp, result );
+
+    if( result.contains( Character.toString(
+                      Markers.ErrorPoint )))
+      {
+      mApp.showStatus( " " );
+      showError = "There was an error marker after" +
+                               " markPreprocessorLines.";
+
+      mApp.showStatus( showError );
+      return "";
+      }
+
+    if( !TestMarkers.testBeginEnd( mApp, result ))
+      {
+      mApp.showStatus( " " );
+      showError = "TestBeginEnd returned false" +
+                       " after markPreprocessorLine.";
+
+      mApp.showStatus( showError );
+      return "";
+      }
+
+    result = MarkupForPreproc.MarkItUp( mApp,
+                                        result );
+
+    mApp.showStatus( result );
+    mApp.showStatus( " " );
+    mApp.showStatus( "Done preprocessing:" );
+    mApp.showStatus( fileName );
+    mApp.showStatus( " " );
+
+    return result;
+    }
+
+
+
+
+  }
+
+
+
