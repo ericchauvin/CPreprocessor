2036d3aad5502fcadfe7bc7fb006f4835168abd1
Eric Chauvin <chauvineric4@gmail.com>
2020-03-05 14:08:10 -0700

    Update PreprocIfLevels.java

--- PreprocIfLevels.java
+++ PreprocIfLevels.java
@@ -1,17 +1,19 @@
 // Copyright Eric Chauvin 2018 - 2020.
 
 
-// This means that I need to get the defined value
-// from a previous macro in this macro.
-// if SWITCHABLE_TARGET
-
+// float a = Float.parseFloat( SomeString );
 
 
   public class PreprocIfLevels
   {
 
+
+  // This has to be broken up in to simpler functions.
   public static String markLevels( String in,
-                                   MainApp mApp )
+                                   MainApp mApp,
+                       DefinesDictionary definesDict )
+    {
+    try
     {
     StringBuilder sBuilder = new StringBuilder();
     StringBuilder lineBuilder = new StringBuilder();
@@ -47,8 +49,6 @@
         if( !levelBool )
           continue;
 
-
-
         sBuilder.append( Markers.Begin );
         sBuilder.append( originalLine );
         continue;
@@ -69,7 +69,7 @@
                     Markers.TypePreprocessor ), "" ); 
 
       boolean commandHasNoBody = command.contains(
-                   Character.toString(Markers.End ));
+                   Character.toString( Markers.End ));
 
       if( commandHasNoBody )
         {
@@ -125,17 +125,59 @@
         }
 
 
+      if( command.equals( "error" ))
+        {
+        if( levelBool )
+          {
+          // It should not get here to an error 
+          // if levelBool is true.
+          // mApp.showStatus( "This is an error." );
+          // return "";
+          }
+        }
+
+      if( command.equals( "define" ))
+        {
+        if( !processDefineStatement( macroBody,
+                                     mApp,
+                                     definesDict ))
+          {
+          return "";
+          }
+        }
+
+      if( command.equals( "undef" ))
+        {
+        mApp.showStatus( " " );
+        mApp.showStatus( "It's a bad idea to use undef." );
+        mApp.showStatus( originalLine );
+        mApp.showStatus( " " );
+        }
+
+      if( command.equals( "include" ))
+        {
+        // Get the dictionary of #define statements
+        // from this included file.
+        }
+
+      if( command.equals( "pragma" ))
+        {
+        }
+
       if( command.equals( "if" ))
         {
         level++;
+/*
         mApp.showStatus( "If line: " + originalLine );
         levelBool = evaluateIfExpression( macroBody );
         if( levelBool )
           {
+          // This means to leave it as-is if 
+          // it's true.
           sBuilder.append( Markers.Begin );
           sBuilder.append( originalLine );
           }
-
+*/
         }
 
       if( command.equals( "ifdef" ))
@@ -144,6 +186,17 @@
         levelBool = true;
         sBuilder.append( Markers.Begin );
         sBuilder.append( originalLine );
+
+/*
+showWarning( )
+This is a function-like macro so it shouldn't be
+used in this ifdef statement.
+Same with ifndef.
+   if( key.contains( "(" ))
+      {
+      mApp.showStatus( " " );
+      mApp.showStatus( "This is a function-like macro." );
+*/
 
         // mApp.showStatus( command + ") " + level );
         }
@@ -185,6 +238,13 @@
         level--;
         if( levelBool )
           {
+/*
+if levelBool is true then this is false.
+  And vice versa.
+
+all the way down to more elifs and else statements.
+*/
+
           sBuilder.append( Markers.Begin );
           sBuilder.append( originalLine );
           }
@@ -219,14 +279,110 @@
       }
 
     return sBuilder.toString();
+
     }
+    catch( Exception e )
+      {
+      mApp.showStatus( "Exception in markLevels()." );
+      mApp.showStatus( e.getMessage() );
+      return "";
+      }
+    }
+
+
+
+  private static boolean processDefineStatement( 
+                                      String in,
+                                   MainApp mApp,
+                       DefinesDictionary definesDict )
+    {
+    try
+    {
+    // mApp.showStatus( " " );
+    // mApp.showStatus( "define statement:" );
+    // mApp.showStatus( in );
+    // mApp.showStatus( " " );
+
+    String[] splitS = in.split( " " );
+    int last = splitS.length;
+    if( last == 0 )
+      {
+      mApp.showStatus( "There is nothing in the define statement." );
+      return false;
+      }
+
+    String key = splitS[0];
+
+    StringBuilder paramBuilder = new StringBuilder();
+
+    if( key.contains( "(" ))
+      {
+      // For keys like this with no space after the
+      // parentheses:
+      // DEF_SANITIZER_BUILTIN_1(ENUM,
+      String[] splitKey = key.split( "(" );
+/*
+      int splitKeyLength = splitKey.length;
+      if( splitKeyLength == 0 )
+        {
+        mApp.showStatus( "This key has no parts." );
+        return false;
+        }
+
+      // Make the parentheses part of the key.
+      key = splitKey[0] + "(";
+
+      if( splitKeyLength > 2 )
+        {
+        mApp.showStatus( "What is the deal here?" );
+        return false;
+        }
+
+      if( splitKeyLength == 2 )
+        paramBuilder.append( splitKey[1] + " " );
+*/
+     
+      // mApp.showStatus( " " );
+      // mApp.showStatus( "This is a function-like macro." );
+      }
+
+
+    for( int count = 1; count < last; count++ )
+      paramBuilder.append( splitS[count] + " " );
+
+    String paramList = paramBuilder.toString();
+    paramList = paramList.trim();
+
+    definesDict.setString( key, paramList );
+
+    mApp.showStatus( " " );
+    mApp.showStatus( "key: " + key );
+    mApp.showStatus( "paramList: " + paramList );
+    mApp.showStatus( " " );
+
+    return true;
+    }
+    catch( Exception e )
+      {
+      mApp.showStatus( "Exception in processDefineStatement()." );
+      mApp.showStatus( e.getMessage() );
+      return false;
+      }
+    }
+
 
 
 
   private static boolean evaluateIfExpression(
                                           String in )
     {
-    in = in.trim();
+    // This means that I need to get the defined
+    // value from a previous macro in this macro.
+    // if SWITCHABLE_TARGET
+
+// definesDict
+
+    // in = in.trim();
     if( in.equals( "0" ))
       return false;
 
