bc6f0aa79e42f0029482b053e846ff0c96cc4780
Eric Chauvin <chauvineric4@gmail.com>
2020-05-27 13:20:28 -0600

    Update Macro.java

--- Macro.java
+++ Macro.java
@@ -1,9 +1,7 @@
 // Copyright Eric Chauvin 2020.
 
 
-// The ## makes two _identifiers_ merge in to one.
-// #define COMMAND(NAME)  { #NAME, NAME ## _command }
-// The single # is for stringizing.
+
 // https://gcc.gnu.org/onlinedocs/gcc-4.8.5/cpp/Stringification.html
 
 
@@ -141,14 +139,15 @@ public class Macro
       return false;
       }
 
-    if( last == 2 )
-      {
-      // Like a #define statement with a key but 
-      // no parameters.
-      // A #define statement can define an empty
-      // macro, like to remove a key word.
-      return true;
-      }
+/*
+Do a warning if there are too many loops to replace
+macros.  Like infinite indirect references.
+What if a macro has the same name as a variable name?
+
+// ==== see if an identifier is self referential.
+// Put in a warning when this happens.
+// And comment the code.
+*/
 
     StringBuilder sBuilder = new StringBuilder();
     for( int count = 2; count < last; count++ )
@@ -161,125 +160,15 @@ public class Macro
     // Put the paramters back, but leave the key out.
     markedUpString = sBuilder.toString();
 
-
-
-/*
-=====
-while( true ) for a different function.
-
-      sBuilder.setLength( 0 );
-
-      for( int count = 2; count < last; count++ )
-        {
-        String token = splitS[count];
-        char firstChar = token.charAt( 0 ); 
-        if( firstChar == Markers.TypeLineNumber )
-          continue;
-
-        if( firstChar != Markers.TypeIdentifier )
-          {
-          if( count == 1 )
-            {
-            mApp.showStatus( "The key is not an identifier." );
-            return false;
-            }
-
-          continue;
-          }
-
-      token = Markers.removeAllMarkers( token );
-      // There might be a space character after
-      // the end marker.
-      token = token.trim();
-      if( count == 1 )
-        {
-        if( !key.equals( token ))
-          {
-          mApp.showStatus( "The key is not equal to the first token." );
-          mApp.showStatus( "Key: >" + key + "<" );
-          mApp.showStatus( "Token: >" + token + "<" );
-          mApp.showStatus( markedUpString );
-          return false;
-          }
-
-        continue;
-        }
-      
-      // If one of these tokens is the same as
-      // the key then it's self referential.
-
-
-      mApp.showStatus( "Token: " + token );
-==== Does the key exist for this token?
-
-====== if( macroDictionary.keyExists( key ))
-      // {
-
-      }
-
-    mApp.showStatus( " " );
-
-
-
-/////////
-    StringBuilder paramBuilder = new StringBuilder();
-    for( int count = 1; count < last; count++ )
-      paramBuilder.append( splitS[count] + " " );
-
-    paramStr = paramBuilder.toString();
-    paramStr = paramStr.trim();
-
-    // The undef, ifdef and ifndef statements all
-    // use a key without the parentheses.  The
-    // parentheses is not part of the key.
-    if( key.contains( "(" ))
+    // How many loops would be abnormal?
+    for( int count = 0; count < 100; count++ )
       {
-      // This is a function-like macro because there
-      // was no space before the first parentheses.
-      // isFunctionType = true;
-
-      // This key could end with the parentheses,
-      // but sometimes they have no space after the
-      // parentheses too like this:
-      // DEF_SANITIZER_BUILTIN_1(ENUM,
-
-      StringArray lineSplitter = new StringArray();
-      int lastPart = lineSplitter.
-                      makeFieldsFromString( key, '(' );
-
-      StringBuilder sBuilder = new StringBuilder();
-
-      key = lineSplitter.getStringAt( 0 );
-      for( int count = 1; count < lastPart; count++ )
-        sBuilder.append( "(" + lineSplitter.
-                                getStringAt( count ));
-
-      paramStr = sBuilder.toString() +
-                              " " + paramStr.trim();
+      if( !replaceMacros())
+        break;
 
       }
 
-    if( macroDictionary.keyExists( key ))
-      {
-      mApp.showStatus( "Macro key already exists: " + key );
-      mApp.showStatus( "paramStr: " + paramStr );
-      Macro showMac = macroDictionary.getMacro( key );
-      mApp.showStatus( "Original paramStr: " +
-                                showMac.getString());
-      return false;
-      }
-    else
-      {
-      macroDictionary.setMacro( key, this );
-      }
-
-    mApp.showStatus( " " );
-    mApp.showStatus( "key: " + key );
-    mApp.showStatus( "paramStr: " + paramStr );
-    mApp.showStatus( " " );
-*/
-
-    return true;
+    return setNewMacroInDictionary();
     }
     catch( Exception e )
       {
@@ -291,13 +180,119 @@ while( true ) for a different function.
 
 
 
-
-/*
-  public String getString()
+  private boolean setNewMacroInDictionary()
     {
-    return key + " " + paramStr;
+    if( macroDictionary.keyExists( key ))
+      {
+      mApp.showStatus( "Macro key already exists: " + key );
+      mApp.showStatus( "markedUpString: " + markedUpString );
+      Macro showMac = macroDictionary.getMacro( key );
+      mApp.showStatus( "Original markedUpString: " +
+                            showMac.getMarkedUpString());
+      return false;
+      }
+
+    macroDictionary.setMacro( key, this );
+    // mApp.showStatus( " " );
+    // mApp.showStatus( "Setting new key: " + key );
+    // mApp.showStatus( "markedUpString: " + markedUpString );
+    // mApp.showStatus( " " );
+    return true;
     }
-*/
+
+
+
+  private boolean replaceMacros()
+    {
+    boolean replacedIdentifier = false;
+
+    StringBuilder sBuilder = new StringBuilder();
+
+    String[] splitS = markedUpString.split(
+                                 Character.toString(
+                                 Markers.Begin ));
+
+    int last = splitS.length;
+      
+    // The string at zero is what's before the first
+    // Begin marker, which is nothing.
+
+    for( int count = 1; count < last; count++ )
+      {
+      String token = splitS[count];
+      char firstChar = token.charAt( 0 ); 
+      if( firstChar == Markers.TypeLineNumber )
+        {
+        sBuilder.append( Character.toString( 
+                                   Markers.Begin ) +
+                                   splitS[count] );
+        continue;
+        }
+
+
+// ====== Fix stringification.
+// Do a search and replace for begin marker
+// the operator then # character and see if it's
+// two in a row.  Put them together as one operator.
+// Do things like >= and operators like that too.
+// Make a SetOperators object?
+
+
+      if( firstChar != Markers.TypeIdentifier )
+        {
+        sBuilder.append( Character.toString( 
+                                   Markers.Begin ) +
+                                   splitS[count] );
+        continue;
+        }
+
+      token = Markers.removeAllMarkers( token );
+      if( token.equals( key ))
+        {
+        mApp.showStatus( "This macro has a self referential key." );
+        mApp.showStatus( "Key: " + key );
+        mApp.showStatus( markedUpString );
+        return false;
+        }
+
+      if( macroDictionary.keyExists( token ))
+        {
+        Macro replaceMacro = macroDictionary.
+                                   getMacro( token );
+
+        replacedIdentifier = true;
+        sBuilder.append( replaceMacro.
+                                getMarkedUpString());
+        continue;
+        }
+
+      sBuilder.append( Character.toString( 
+                       Markers.Begin ) + splitS[count] );
+
+      }
+
+    markedUpString = sBuilder.toString();
+
+    if( !MarkupString.testMarkers( markedUpString, 
+                                   "replaceMacros().",
+                                   mApp ))
+      {
+      return false;
+      }
+
+    if( replacedIdentifier )
+      mApp.showStatus( "markedUpString: " + markedUpString );
+
+    return replacedIdentifier; 
+    }
+
+
+
+  public String getMarkedUpString()
+    {
+    return markedUpString;
+    }
+
 
 
   public String getKey()
