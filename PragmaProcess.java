98c17c2ac8e041a39eb09384fa86e613fe2f3218
Eric Chauvin <chauvineric4@gmail.com>
2020-06-20 12:37:57 -0600

    Create PragmaProcess.java

---
+++ PragmaProcess.java
@@ -0,0 +1,40 @@
+// Copyright Eric Chauvin 2020.
+
+
+
+public class PragmaProcess
+  {
+/*
+#pragma: warning(push)  20;winapifamily.h 
+#pragma: warning(disable:4001)    21;winapifamily.h 
+#pragma: once  23;winapifamily.h 
+#pragma: warning(push)  20;winpackagefamily.h 
+#pragma: warning(disable:4001)    21;winpackagefamily.h 
+#pragma: once  23;winpackagefamily.h 
+#pragma: warning(pop)  87;winpackagefamily.h 
+#pragma: warning(pop)  247;winapifamily.h 
+#pragma: warning(push)  20;sdkddkver.h 
+#pragma: warning(disable:4668)    21;sdkddkver.h 
+#pragma: warning(disable:4001)    23;sdkddkver.h 
+#pragma: once  26;sdkddkver.h 
+#pragma: warning(pop)  303;sdkddkver.h 
+#pragma: warning(default:4001)    305;sdkddkver.h 
+#pragma: once  28;Windows.h 
+#pragma: region Application Family or OneCore Family  31;Windows.h 
+#pragma: warning(disable:4116)          148;Windows.h 
+#pragma: warning(disable:4514)  156;Windows.h 
+#pragma: warning(disable:4103)  158;Windows.h 
+#pragma: warning(push)  161;Windows.h 
+#pragma: warning(disable:4001)  163;Windows.h 
+#pragma: warning(disable:4201)  164;Windows.h 
+#pragma: warning(disable:4214)  165;Windows.h 
+#pragma: warning(pop)  254;Windows.h 
+#pragma: warning(default:4001)  256;Windows.h 
+#pragma: warning(default:4201)  257;Windows.h 
+#pragma: warning(default:4214)  258;Windows.h 
+#pragma: endregion  267;Windows.h 
+
+
+*/
+
+  }
