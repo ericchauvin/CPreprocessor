29273ea6211a6b303f7494226acb159de9f94baf
Eric Chauvin <chauvineric4@gmail.com>
2020-07-02 11:52:25 -0600

    Update IfExpression.java

--- IfExpression.java
+++ IfExpression.java
@@ -8,15 +8,15 @@ public class IfExpression
 
 
 
-  public static String evaluateExpression(
+  public static StrA evaluateExpression(
                                          MainApp mApp,
                                          StrA expr,
                      MacroDictionary macroDictionary )
     {
     if( expr == null )
-      return "";
+      return new StrA( "" );
 
-    String originalExpr = expr.toString();
+    StrA originalExpr = expr;
     // Remove line number markers.
     expr = expr.removeSections( Markers.Begin,
                                         Markers.End );
@@ -25,52 +25,54 @@ public class IfExpression
     if( expr.length() == 0 )
       {
       mApp.showStatusAsync( "If expression is empty." );
-      return "";
+      return new StrA( "" );
       }
 
     if( expr.equals( "1" ))
-      return "true";
+      return new StrA( "true" );
 
     if( expr.equals( "0" ))
-      return "false";
+      return new StrA( "false" );
 
-    String markedUp = MarkupString.MarkItUp( 
-                              mApp, expr.toString() ).toString();
+    StrA markedUp = MarkupString.MarkItUp( 
+                              mApp, expr );
 
     if( markedUp.length() == 0 )
-      return "";
+      return new StrA( "" );
 
     // mApp.showStatusAsync( " " );
     // mApp.showStatusAsync( markedUp );
-    if( markedUp.contains( "defined" ))
+    if( markedUp.contains( new StrA( "defined" )))
       {
       markedUp = setDefineValues( mApp,
                                   macroDictionary,
                                   markedUp );
 
       if( markedUp.length() == 0 )
-        return "";
+        return new StrA( "" );
 
-      if( !MarkupString.testMarkers( markedUp, "After setDefineValues().", mApp ))
-        return "";
+      if( !MarkupString.testMarkers( markedUp,
+            new StrA(  "After setDefineValues()." ),
+                                     mApp ))
+        return new StrA( "" );
 
       }
 
     // mApp.showStatusAsync( markedUp );
 
     if( markedUp.equals( BoolExp.TrueA ))
-      return "true";
+      return new StrA( "true" );
 
     if( markedUp.equals( BoolExp.FalseA ))
-      return "false";
+      return new StrA( "false" );
 
     markedUp = BoolExp.evaluate( mApp, markedUp );
 
-    if( markedUp.equals( BoolExp.TrueA.toString() ))
-      return "true";
+    if( markedUp.equals( BoolExp.TrueA ))
+      return new StrA( "true" );
 
-    if( markedUp.equals( BoolExp.FalseA.toString() ))
-      return "false";
+    if( markedUp.equals( BoolExp.FalseA ))
+      return new StrA( "false" );
 
 
 
@@ -82,10 +84,10 @@ public class IfExpression
                                     macroDictionary );
 
     if( markedUp.equals( BoolExp.TrueA ))
-      return "true";
+      return new StrA( "true" );
 
     if( markedUp.equals( BoolExp.FalseA ))
-      return "false";
+      return new StrA( "false" );
 
 
     // 100 != 100 && 100 != 2 && 100 != 3 &&
@@ -103,28 +105,30 @@ public class IfExpression
 
     // mApp.showStatusAsync( markedUp );
 
-    return "false";
+    return new StrA( "false" );
     }
 
 
 
 
-  private static String setDefineValues( MainApp mApp,
+  private static StrA setDefineValues( MainApp mApp,
                       MacroDictionary macroDictionary,
-                                     String markedUp )
+                                     StrA markedUp )
     {
+    // Really strings for this part.
+
     final String DefinedPart = "" + 
                          Markers.Begin + 
                          Markers.TypeIdentifier +
                          "defined" + 
                          Markers.End;
  
-    final String TrueIn1Str = DefinedPart +
-                         BoolExp.TrueA.toString();
+    final String TrueIn1Str = DefinedPart + 
+                              BoolExp.TrueA;
 
 
     final String TrueIn2Str = DefinedPart +
-                         Markers.Begin +
+                         "" + Markers.Begin +
                          Markers.TypeOperator +
                          "(" +
                          Markers.End +
@@ -153,25 +157,25 @@ public class IfExpression
     StrA falseIn1 = new StrA( FalseIn1Str );
     StrA falseIn2 = new StrA( FalseIn2Str );
 
-
-    String[] exprParts = markedUp.split( "" + 
+    StrArray exprParts = markedUp.splitChar(  
                                      Markers.Begin );
 
-    int last = exprParts.length;
+    final int last = exprParts.length();
     boolean isInside = false;
-    StringBuilder sBuilder = new StringBuilder();
+    StrABld sBuilder = new StrABld( 1024 );
     // The value at zero is before the first
     // Begin marker.  Which is nothing.
     // Starts at 1:
     for( int count = 1; count < last; count++ )
       {
-      String part = exprParts[count];
+      StrA part = exprParts.getStrAt( count );
 
-      if( part.startsWith( "" + 
+      if( part.startsWith( new StrA( "" + 
                            Markers.TypeIdentifier +
-                           "defined" ))
+                           "defined" )))
         {
-        sBuilder.append( "" + Markers.Begin + part );
+        sBuilder.appendStrA( new StrA(
+                        "" + Markers.Begin + part ));
         isInside = true;
         continue;
         }
@@ -180,30 +184,31 @@ public class IfExpression
         {
         // The first identifier after the 'defined'
         // key word.
-        if( part.startsWith( "" + 
-                             Markers.TypeIdentifier ))
+        if( part.startsWith( new StrA( "" + 
+                             Markers.TypeIdentifier )))
           {
           isInside = false;
-          String macroName = Markers.removeAllMarkers( 
+          StrA macroName = Markers.removeAllMarkers( 
                                               part );
 
           if( macroDictionary.keyExists( macroName ))
             {
-            sBuilder.append( BoolExp.TrueA.toString() );
+            sBuilder.appendStrA( BoolExp.TrueA );
             }
           else
             {
-            sBuilder.append( BoolExp.FalseA.toString() );
+            sBuilder.appendStrA( BoolExp.FalseA );
             }
 
           continue;
           }
         }
 
-      sBuilder.append( "" + Markers.Begin + part );
+      sBuilder.appendStrA( new StrA(
+                        "" + Markers.Begin + part ));
       }
 
-    StrA result = new StrA( sBuilder.toString());
+    StrA result = sBuilder.toStrA();
 
     // mApp.showStatusAsync( "\n\nBefore replace:\n" + result.toString() );
 
@@ -223,15 +228,15 @@ public class IfExpression
     // mApp.showStatusAsync( "\n\nfalsein1: " + result.toString() );
 
 
-    if( !MarkupString.testBeginEnd( mApp, result.toString() ))
+    if( !MarkupString.testBeginEnd( mApp, result ))
       {
       mApp.showStatusAsync( "IfExpression. The string isn't right: " + result );
-      return "";
+      return new StrA( "" );
       }
 
     // mApp.showStatusAsync( "\n\n" + result.toString() );
 
-    return result.toString();
+    return result;
     }
 
 
