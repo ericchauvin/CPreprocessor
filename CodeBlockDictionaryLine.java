cbc6fabe07fbc7be8b6fb38fa1c37caf06d77440
Eric Chauvin <chauvineric4@gmail.com>
2020-07-07 11:19:25 -0600

    Create CodeBlockDictionaryLine.java

---
+++ CodeBlockDictionaryLine.java
@@ -0,0 +1,182 @@
+// Copyright Eric Chauvin 2020.
+
+
+
+public class CodeBlockDictionaryLine
+  {
+  private int[] keyArray;
+  private StrA[] valueArray;
+  private int[] sortIndexArray;
+  private int arrayLast = 0;
+
+
+
+  public CodeBlockDictionaryLine()
+    {
+    keyArray = new int[8];
+    valueArray = new StrA[8];
+    sortIndexArray = new int[8];
+    resetSortIndexArray();
+    }
+
+
+
+  private void resetSortIndexArray()
+    {
+    // It's not to arrayLast.  It's to the whole length.
+    final int max = sortIndexArray.length;
+    for( int count = 0; count < max; count++ )
+      sortIndexArray[count] = count;
+
+    }
+
+
+
+  private void resizeArrays( int toAdd )
+    {
+    int max = sortIndexArray.length;
+    sortIndexArray = new int[max + toAdd];
+    resetSortIndexArray();
+
+    int[] tempKeyArray = new int[max + toAdd];
+    StrA[] tempValueArray = new StrA[max + toAdd];
+
+    for( int count = 0; count < arrayLast; count++ )
+      {
+      tempKeyArray[count] = keyArray[count];
+      tempValueArray[count] = valueArray[count];
+      }
+
+    keyArray = tempKeyArray;
+    valueArray = tempValueArray;
+    }
+
+
+/*
+  public void sort()
+    {
+    if( arrayLast < 2 )
+      return;
+
+    for( int count = 0; count < arrayLast; count++ )
+      {
+      if( !bubbleSortOnePass() )
+        break;
+
+      }
+    }
+*/
+
+
+/*
+  private boolean bubbleSortOnePass()
+    {
+    // This returns true if it swaps anything.
+
+    boolean switched = false;
+    for( int count = 0; count < (arrayLast - 1); count++ )
+      {
+      // compareTo() uses case.
+      if( keyArray[count].compareToIgnoreCase(
+                              keyArray[count + 1] ) > 0 )
+        {
+        int temp = sortIndexArray[count];
+        sortIndexArray[count] = sortIndexArray[count + 1];
+        sortIndexArray[count + 1] = temp;
+        switched = true;
+        }
+      }
+
+    return switched;
+    }
+*/
+
+
+  private int getIndexOfKey( int key )
+    {
+    if( arrayLast < 1 )
+      return -1;
+
+    for( int count = 0; count < arrayLast; count++ )
+      {
+      if( keyArray[count] == key )
+        return count;
+
+      }
+
+    return -1;
+    }
+
+
+
+  public void setValue( int key, StrA value )
+    {
+    int index = getIndexOfKey( key );
+    if( index >= 0 )
+      {
+      valueArray[index] = value;
+      }
+    else
+      {
+      if( arrayLast >= sortIndexArray.length )
+        resizeArrays( 1024 * 32 );
+
+      keyArray[arrayLast] = key;
+      valueArray[arrayLast] = value;
+      arrayLast++;
+      }
+    }
+
+
+
+  public StrA getValue( int key )
+    {
+    int index = getIndexOfKey( key );
+    if( index < 0 )
+      return StrA.Empty;
+
+    return valueArray[index];
+    }
+
+
+
+
+  public boolean keyExists( int key )
+    {
+    int index = getIndexOfKey( key );
+    if( index < 0 )
+      return false;
+
+    return true;
+    }
+
+
+
+/*
+  public StrA makeKeysValuesStrA()
+    {
+    if( arrayLast < 1 )
+      return StrA.Empty;
+
+    StrABld sBuilder = new StrABld( 1024 * 32 );
+
+    for( int count = 0; count < arrayLast; count++ )
+      {
+      // Using the sortIndexArray for the sorted order.
+      StrA oneLine = new StrA( 
+              keyArray[sortIndexArray[count]] );
+
+      oneLine = oneLine.concat( new StrA( ";" ));
+      oneLine = oneLine.concat( 
+                valueArray[sortIndexArray[count]] );
+      oneLine = oneLine.concat( new StrA( "\n" ));
+
+      sBuilder.appendStrA( oneLine );
+      }
+
+    return sBuilder.toStrA();
+    }
+*/
+
+
+  }
