3010dcaf75a0c1c9b97dfc303b0b538f53dd47ad
Eric Chauvin <chauvineric4@gmail.com>
2020-07-31 13:11:47 -0600

    Create CppReservedWords.java

---
+++ CppReservedWords.java
@@ -0,0 +1,127 @@
+// Copyright Eric Chauvin 2020.
+
+
+
+public class CppReservedWords
+  {
+  private final static String[] reservedWords = {
+      "alignas",
+      "alignof",
+      "and",
+      "and_eq",
+      "asm",
+      "atomic_cancel",
+      "atomic_commit",
+      "atomic_noexcept",
+      "auto",
+      "bitand",
+      "bitor",
+      "bool",
+      "break",
+      "case",
+      "catch",
+      "char",
+      "char8_t",
+      "char16_t",
+      "char32_t",
+      "class",
+      "compl",
+      "concept",
+      "const",
+      "consteval",
+      "constexpr",
+      "constinit",
+      "const_cast",
+      "continue",
+      "co_await",
+      "co_return",
+      "co_yield",
+      "decltype",
+      "default",
+      "delete",
+      "do",
+      "double",
+      "dynamic_cast",
+      "else",
+      "enum",
+      "explicit",
+      "export",
+      "extern",
+      "false",
+      "float",
+      "for",
+      "friend",
+      "goto",
+      "if",
+      "inline",
+      "int",
+      "long",
+      "mutable",
+      "namespace",
+      "new",
+      "noexcept",
+      "not",
+      "not_eq",
+      "nullptr",
+      "operator",
+      "or",
+      "or_eq",
+      "private",
+      "protected",
+      "public",
+      "reflexpr",
+      "register",
+      "reinterpret_cast",
+      "requires",
+      "return",
+      "short",
+      "signed",
+      "sizeof",
+      "static",
+      "static_assert",
+      "static_cast",
+      "struct",
+      "switch",
+      "synchronized",
+      "template",
+      "this",
+      "thread_local",
+      "throw",
+      "true",
+      "try",
+      "typedef",
+      "typeid",
+      "typename",
+      "union",
+      "unsigned",
+      "using",
+      "virtual",
+      "void",
+      "volatile",
+      "wchar_t",
+      "while",
+      "xor",
+      "xor_eq",
+      "final",
+      "override",
+      "transaction_safe",
+      "transaction_safe_dynamic" };
+    
+
+
+  public static boolean isReserved( String in )
+    {
+    final int last = reservedWords.length;
+    for( int count = 0; count < last; count++ )
+      {
+      if( in.compareTo( reservedWords[count] ) == 0 )
+        return true;
+
+      }
+
+    return false;
+    }
+
+
+
+  }
